6b8212412f0513e1e69c73ad7803247d4cd69d61
eckrin <bsh51515@naver.com>
2022-10-02 20:23:02 +0900

    초기설정

---
+++ src/test/kotlin/yourssu/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package yourssu.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
